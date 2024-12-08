a7edb514bd33a25933a329bb86254374ec37723e
Parag Kumar Goyal <1997parag.goyal@gmail.com>
2024-12-08 17:18:10 +0530

    initial setup installed react-router, react-icons, configured theme colors and fonts, add basic project structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router";
+import Navbar from "./components/Navbar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
+        <Outlet />
+      </main>
+      <footer>Footer!</footer>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Navbar = () => {
+  // max-w-screen-2xl equivalent to max-width: 1536px in css
+  return <header className="max-w-screen-2xl mx-auto px-4 py-6"></header>;
+};
+
+export default Navbar;
